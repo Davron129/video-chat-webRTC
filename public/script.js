b64b1a06ba706721503366839ee455a9ff060bdd
Davron Normunminov <normominovdavron001@gmail.com>
2021-12-04 19:36:02 +0500

    asdasdf

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: false
